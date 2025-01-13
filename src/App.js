1b5266d96f6802078edea211ac43c5807c20b862
Uday Solanki <Udaysolanki530@gmail.com>
2025-01-13 18:53:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandignPage from './Landing Page/Page'
+
+function App() {
+  return (
+    <div>
+      <LandignPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Landing Page/Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LandignPage() {
+  return (
+    <div>LandignPage</div>
+  )
+}
+
+export default LandignPage;
